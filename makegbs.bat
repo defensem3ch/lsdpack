f4af8d8946ef16aec5f3af07dd486db108cc7845
DEFENSE MECHANISM <72157321+defensem3ch@users.noreply.github.com>
2021-02-26 11:25:51 -0500

    Update makegbs.bat

--- makegbs.bat
+++ makegbs.bat
@@ -1,22 +1,36 @@
 @echo off
 setlocal enabledelayedexpansion
-for /f "delims=" %%a in ('dir /b lsdpack*exe') do set lsdpack=!a!
-echo !a!
-for /f "delims=" %%b in ('dir /b makegbs*exe') do set makegbs=!b!
-echo !b!
-if not exist !lsdpack! (echo Please place lsdpack.exe in this folder && exit)
-if not exist !makegbs! (echo Please place makegbs.exe in this folder && exit)
-if not exist rgbasm.exe (echo Please place rgbasm.exe in this folder && exit)
-if not exist rgblink.exe (echo Please place rgblink.exe in this folder && exit)
+for /f "delims=" %%a in ('dir /b lsdpack*exe') do set lsdpack=%%a%
+echo Found lsdpack: !lsdpack!
+for /f "delims=" %%b in ('dir /b makegbs*exe') do set makegbs=%%b%
+echo Found makegbs: !makegbs!
+if not exist !lsdpack! (echo Please place lsdpack.exe in this folder && goto end)
+if not exist !makegbs! (echo Please place makegbs.exe in this folder && goto end)
+if not exist rgbasm.exe (echo Please place rgbasm.exe in this folder && goto end)
+if not exist rgblink.exe (echo Please place rgblink.exe in this folder && goto end)
 if [%1]==[] (
  echo Usage: makegbs.bat lsdj.gb
  echo Please specify an LSDJ ROM
+ goto end
+) else (
+ set lsdj=%1%
+ echo Compiling song from !lsdj!
+ if exist !lsdj!*s rm !lsdj!*s
 )
-set sav=%1:.gb=.sav%
-set rom=%1:.gb=%
-if not exist !sav! (echo Please place %1 and !sav! in this folder && exit)
+set sav=!lsdj:.gb=.sav!
+set rom=!lsdj:.gb=!
+if not exist !sav! (echo Please place %1 and !sav! in this folder && goto end)
 if not exist player.o (rgbasm.exe -o player.o player.s)
-lsdpack.exe -g %1
-rgbasm.exe -o !rom!.o !rom!.s
-rgblink.exe -o !rom!-player.gb player.o !rom!.o
-makegbs.exe !rom!-player.gb
+!lsdpack! -g %1
+for /f "delims=" %%c in ('dir /b !rom!*.s') do (
+ set in=%%c%
+ set out=!in:.s=.o!
+ rgbasm -o !out! !in!
+)
+for /f "delims=" %%d in ('dir /b !rom!*.o') do (
+ set in=%%d%
+ set out=!in:.o=!
+ rgblink -o !out!-player.gb player.o !in!
+)
+for /f "delims=" %%e in ('dir /b *-player.gb') do !makegbs! %%e
+:end
